13a6a5dc63255990b0c1298bb680f2b25e7511e2
Rakesh-Dasi <dasi.rakesh93@yahoo.com>
2017-12-04 15:03:32 +0530

    First version of WebDriverIO code

---
+++ test/test.js
@@ -0,0 +1,39 @@
+var assert = require('assert');
+var request = require('request');
+
+describe('webdriver.io page', function(){
+	it('should have the right title', function(){
+		browser.url('/');
+		var title = browser.getTitle();
+		assert.equal(title, 'WebdriverIO - WebDriver bindings for Node.js');
+	});
+
+	describe('Api Page', function () {
+
+	it('should have a link to it from the homepage', function (){
+		browser.url('/');
+		var hasApiLink = browser.isExisting('=API');
+		assert(hasApiLink);
+		//browser.isExisting('a[href="/api.html"]');
+
+	});
+
+	it('should take you to the API page', function (){
+		browser.url('/');
+		browser.click('=API');
+		var title = browser.getTitle();
+		assert.equal(title, 'WebdriverIO - API Docs');
+
+		//To pause for 2 sec
+		browser.pause(2000);
+	});
+
+	it('should filter search results', function (){
+		browser.url('/api.html');
+		
+		browser.setValue('input[name="search"]','debug');
+		browser.saveScreenshot('api-with-result.png');
+
+	});
+});
+});
